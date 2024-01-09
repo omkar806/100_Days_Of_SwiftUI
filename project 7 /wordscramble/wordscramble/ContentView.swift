84492ed49dba8abde2ea1ad1b26fe97f3595a2fe
Omkar Malpure <77787482+omkar806@users.noreply.github.com>
2024-01-09 19:09:16 +0530

    Project 7 WordScramble

---
+++ project 7 /wordscramble/wordscramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Omkar Malpure on 02/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project 7 /wordscramble/wordscramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Omkar Malpure on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
