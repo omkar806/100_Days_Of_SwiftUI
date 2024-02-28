5667f2675132baddd4c8fa03349e0fa57e66839e
omkar806 <malpureomkar5@gmail.com>
2024-02-28 14:39:30 +0530

    adding project iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Omkar Malpure on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
